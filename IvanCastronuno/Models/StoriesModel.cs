47d7ca8d3ef464d9a8934bb04e8a801ddc7db589
ivanreith <castronunoi@my.lanecc.edu>
2021-01-07 19:52:59 -0800

    All working , but the validation doesn't allow me to overflow the fields, so the only error that I can show is field required

--- IvanCastronuno/Models/StoriesModel.cs
+++ IvanCastronuno/Models/StoriesModel.cs
@@ -12,25 +12,28 @@ namespace IvanCastronuno.Models
         // EF Core will configure the database to generate this value
         [Key]
         public int StoryID { get; set; }
-        // [Required]
-        //  [MaxLength(50, ErrorMessage ="Title between 1 and 50 chars")]
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="Title between 1 and 50 chars")]
         public string StoryTitle { get; set; }
-      //  [Required]
-       // [MaxLength(25, ErrorMessage = "storyTopic between 1 and 25 chars")]
+
+        [Required]
+        [MaxLength(25, ErrorMessage = "storyTopic between 1 and 25 chars")]
         public string StoryTopic { get; set; }
-      //  [Required]
-      //  [MaxLength(250, ErrorMessage = "storyText between 1 and 250 chars")]
+
+        [Required]
+        [MaxLength(250, ErrorMessage = "storyText between 1 and 250 chars")]
         public string StoryText { get; set; }
-        //  [Required]
-        //  [MaxLength(250, ErrorMessage = "storyTeller between 1 and 250 chars")]
+        
         public string UserId { get; set; }
         public User Poster { get; set; }       
-         
-        //public DateTime StoryDate { get; set; }
+        
+        public DateTime StoryTime { get; set; }
 
         public string Slug =>
-         StoryTitle?.Replace(' ', '-').ToLower() + '-' + StoryTopic.ToString();
+         StoryTitle?.Replace(' ', '-').ToLower() + '-' + StoryTopic?.ToString();
 
     }
     
 }
+  
